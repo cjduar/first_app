95d1e8ea765dfd2b62140ba1af36edd455ff59f1
Christopher Duarte <cjduar@gmail.com>
2013-06-20 15:35:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstAppHartl::Application.config.secret_token = '2cfd689fd22730b807df78838bff391db03a0bbde8638f56ca01cb2f37232b9e10234fd0aab9b0679b2e36c747787fc801555f624f03eb64157eddb5f4b540b7'
